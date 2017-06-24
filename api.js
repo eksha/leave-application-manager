ae4f7bd4b6846a7ea4cc1e994ff718cabb52d4ae
Dee <deekshagupta726@gmail.com>
2017-06-24 22:48:43 +0530

    Update API to include authorization and accessbility of Employees and Managers -if username does not exist in database, do not allow any kind of access -if username has role Employee, allow to view their own applications(GET) and submit new applications(POST) -if username has role Manager, allow to view all the applications in sytem(GET) and approve any application(PUT)

--- api.js
+++ api.js
@@ -5,28 +5,80 @@ var db=require('./database.js');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+app.use('/static',express.static('static'));
 
 //api to get and post leave application for employees
-app.get('/employee/:username',function(req,res){
+app.get('/:username/',function(req,res){
+    //check whether the username exists in the database
+    db.user.findOne({username:req.params.username},function(err,doc){
+      if(err){
+        console.log(err);
+        res.json({code:-1,message:"Could not retrieve Leave Applications"});
+      }
+      //if not registered user, don't allow access
+      if(doc.length<1){
+        console.log("Unauthentic User");
+        res.json({code:-1,message:"Unauthentic Username!"});
+      }
+      //if registered user
+      else{
+        console.log(doc);
+        //if role is Employee, return the list of previous leave applications
+        if(doc.role=="Employee"){
+          db.leave.find({requestedBy:req.params.username},function(err,appl){
+            if(err){
+            console.log(err);
+            res.json({code:-1,message:"Could not retrieve Leave Applications"});
+            }
 
-   db.leave.find({requestedBy:req.params.username},function(err,doc){
-     if(err){
-     console.log(err);
-     res.json({code:-1,message:"Could not retrieve Leave Applications"});
-     }
-     if(doc.length>0){
-       console.log(doc);
-       res.json(doc);
-     }
-     else {
-       res.json({code:1,message:"No Leave Application to show!"});
-     }
-   });
+            else {
+              if(appl.length>0){
+              console.log(appl);
+              res.json(appl);
+            }
+            else {
+              res.json({code:1,message:"No Leave Application to show!"});
+            }
+          }
+          });
+        }
+        //if role is Manager, return the entire list of leave applications
+        else if(doc.role=="Manager"){
+          db.leave.find().sort({requestedAt:-1}).exec(function(err,applications){
+            if(err){
+              console.log(err);
+              res.json({code:-1,message:"Could not retrieve leave application"});
+            }
+            else{
+              console.log(applications);
+              res.json(applications);
+            }
+        });
+      }
+      }
+    });
 });
 
 
-app.post('/employee/:username',function(req,res){
+app.post('/:username',function(req,res){
+  //check whether the username exists in the database
+  db.user.findOne({username:req.params.username},function(err,doc){
+    if(err){
+      console.log(err);
+      res.json({code:-1,message:"Internal Error"});
+    }
+    //if not registered user, don't allow access
+    else if(doc===null){
+      console.log("Unauthentic User");
+      res.json({code:-1,message:"Unauthentic Username!"});
+    }
+    //if role is Manager, refrain from making POST request
+    else if(doc.role=="Manager"){
+      console.log("Not authorized");
+      res.json({code:-1,message:"Not authorized to make POST request"});
+    }
+    //if role is Employee, allow POST request
+    else{
   console.log(req.body);
   var newleave = new db.leave(req.body);
   newleave.requestedBy=req.params.username;
@@ -37,33 +89,37 @@ app.post('/employee/:username',function(req,res){
       res.json({code:-1,message:"Could not Post the leave Application"});
       }
     else {
-      console.log("post Successful");
+      console.log("Post Successful");
       res.json({code:1,message:"Leave Application Posted Succesfully"})
      }
-
   });
+}
 
 });
+});
 
-//api to get  and approve leave application for manager
+//api to approve leave application for manager
 
-app.get('/manager',function(req,res){
-
-  db.leave.find().sort({requestedAt:-1}).exec(function(err,applications){
+app.put('/:username/:id',function(req,res){
+    //check whether the username exists in the database
+  db.user.findOne({username:req.params.username},function(err,doc){
     if(err){
       console.log(err);
-      res.json({code:-1,message:"Could not retrieve leave application"});
+      res.json({code:-1,message:"Internal Error"});
     }
+    //if not registered user, don't allow access
+    else if(doc===null){
+      console.log("Unauthentic User");
+      res.json({code:-1,message:"Unauthentic Username!"});
+    }
+    //if role is Employee, refrain from making PUT request
+    else if(doc.role=="Employee"){
+      console.log("Not authorized");
+      res.json({code:-1,message:"Not authorized to make PUT request"});
+    }
+    //if role is Manager, allow PUT request
     else{
-      console.log(applications);
-      res.json(applications);
-    }
-});
-});
-
-app.put('/manager/:id',function(req,res){
-
-db.leave.update({_id:req.params.id},{approvalStatus:true,approvedAt:new Date()},function(err){
+      db.leave.update({_id:req.params.id},{approvalStatus:true,approvedAt:new Date()},function(err){
   if(err){
     console.log(err);
     res.json({code:-1,mesage:"Could not approve leave Application"});
@@ -74,7 +130,8 @@ db.leave.update({_id:req.params.id},{approvalStatus:true,approvedAt:new Date()},
   }
 
 });
-
+}
+});
 });
 
 module.exports=app;
