d1dfa93dcdbd62dce32a7e3470dcd6d092704030
Dee <deekshagupta726@gmail.com>
2017-06-25 23:40:27 +0530

    Fix port issue - provide process.env.PORT as listening port for heroku app

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var app = require('./api.js');
 
 var port = 8092;
 
-http.createServer(app).listen(port);
+http.createServer(app).listen(process.env.PORT||port);
 
 console.log("Listening on port "+ port);
